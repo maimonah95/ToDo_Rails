32bbb324980f01da883a97d8cab210734f7f3afc
maimonah95 <malhwaymil@gmail.com>
2020-03-30 12:09:19 +0300

    Read - index,create Todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,19 @@ class TodosController < ApplicationController
     def show
     @todo = Todo.find(params[:id])
     end
+    def index 
+      @todos = Todo.all  
+    end
+    def new
+        @todo = Todo.new
+    end
+    def create 
+        @todo = Todo.new(todo_params)
+        @todo.save
+        redirect_to @todo
+    end
+    private 
+    def todo_params
+        params.require(:todo).permit(:title,:completed)
+    end
 end
